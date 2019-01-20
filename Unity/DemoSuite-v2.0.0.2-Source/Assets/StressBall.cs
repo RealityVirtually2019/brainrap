22d876f8fb0ec5282c92977936e78ecfdd39558c
Adam <adamsauer.ee@gmail.com>
2019-01-19 19:04:04 -0500

    Add Stress particle system

---
+++ Unity/DemoSuite-v2.0.0.2-Source/Assets/StressBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class StressBall : MonoBehaviour {
+
+        [Header("Passes Stress State Values into the Associated Slider Object")]
+        [SerializeField]
+        SphereCollider stressBall;
+
+        SphereCollider BallObject
+        {
+            get
+            {
+                if (stressBall == null) stressBall = GetComponent<SphereCollider>();
+                if (stressBall == null) stressBall = GetComponentInChildren<SphereCollider>();
+                return stressBall;
+            }
+        }
+
+        // NeurableAffectiveStateEngine returns a tuple of <time, value>.
+        // This function takes the second value and passes it to the Slider
+        public void UpdateBallWithStressState(float timestamp, float value)
+        {
+            if (BallObject != null)
+                BallObject.transform.position = new Vector3(-1, value * 10+2, 0);
+        }
+    }
